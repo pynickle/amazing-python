fadeb4e57795670f8a093802fc1ae2f2371fe186
code-nick-python <47242104+code-nick-python@users.noreply.github.com>
2019-04-18 13:14:03 +0800

    update

--- pip_update/pip_all.py
+++ pip_update/pip_all.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import pip
-from subprocess import call
-from pip._internal.utils.misc import get_installed_distributions
-update_now=1
-package_number=len(get_installed_distributions())
-for dist in get_installed_distributions():
-    try:
-        call("pip install --upgrade " + dist.project_name, shell=True)
-        print("all package:{},update now:{}".format(package_number,update_now))
-        update_now+=1
-    except:
-        continue
-print("all is finished:{}".format(s))
-=======
 import pip
 from subprocess import call
 from pip._internal.utils.misc import get_installed_distributions
@@ -28,4 +13,3 @@ for dist in get_installed_distributions():
     except:
         continuer
 print("all is finished:{}".format(s))
->>>>>>> A
--- zip_to_see/zip.py
+++ zip_to_see/zip.py
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import zipfile
-import tkinter
-from tkinter import filedialog
-import tkinter.messagebox
-from pathlib import Path
-
-top=tkinter.Tk()
-
-def un_zip(file_name):
-    i=0
-    try:
-        path_use=file_choose_1.get()
-        with zipfile.ZipFile(path_use, 'r') as f:
-            for fn in f.namelist():
-                i+=1
-                extracted_path = Path(f.extract(fn))
-                try:
-                    extracted_path.rename(fn.encode('cp437').decode('gbk'))
-                except:
-                    tkinter.messagebox.showwarning('wrong!','wrong zip!')
-            tkinter.messagebox.showinfo('success!','finish zip '+str(i)+'!')
-    except:
-        tkinter.messagebox.showwarning('wrong!','wrong zip!')
-
-path=tkinter.StringVar()
-
-def choose_file():
-    path_=filedialog.askopenfilename(filetypes=[('zip files', '*.zip')])
-    path.set(path_)
-
-while True:
-    button_begin=tkinter.Button(top,text='begin',command=lambda:un_zip(file_name=path))
-    button_begin.pack()
-    button=tkinter.Button(top,text='choose zip file',command=choose_file)
-    button.pack()
-    file_choose_1=tkinter.Entry(top,textvariable=path,width=50)
-    file_choose_1.pack()
-    top.mainloop()
-=======
 import zipfile
 import tkinter
 from tkinter import filedialog
@@ -83,4 +43,3 @@ while True:
     file_choose_1 = tkinter.Entry(top, textvariable=path, width=50)
     file_choose_1.pack()
     top.mainloop()
->>>>>>> A
