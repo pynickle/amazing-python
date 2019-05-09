3f976076c9519c3ded1086015968c24c7ef141dd
code-nick-python <2330458484@qq.com>
2019-05-09 19:50:41 +0800

    Add more

--- study_program/builtin_function.py
+++ study_program/builtin_function.py
@@ -17,4 +17,11 @@ print("type(5.5) : " + str(type(5.5)) + "\n")
 
 #获取长度
 print("len('Hello World!') : " + str(len('Hello World!')))
-print("len([1,2,3,4,5]) : " + str(len([1,2,3,4,5])))
+print("len([1,2,3,4,5]) : " + str(len([1,2,3,4,5])) + "\n")
+
+#排序
+print("sorted([3,4,2,1,5]) : " + str(sorted([3,4,2,1,5]))+"\n")
+
+#求和
+print("sum((1,2,3,4,5,6)) : " + str(sum((1,2,3,4,5,6))))
+print("sum([[1,2],[3,4],[5,6]],[]) : " + str(sum([[1,2],[3,4],[5,6]],[])) + "\n")
