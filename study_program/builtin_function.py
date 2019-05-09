42f3b14db1e2ccfe278ef688829c8c17f8473a1b
code-nick-python <2330458484@qq.com>
2019-05-09 17:56:44 +0800

    内置函数

---
+++ study_program/builtin_function.py
@@ -0,0 +1,14 @@
+#绝对值
+print("abs(-10) : "+str(abs(-10))+"\n")
+
+#遍历下标和元素
+print("In ['first','second','third']")
+for i,num in enumerate(["first","second","third"]):
+	print(i,num)
+print()
+
+#判断类型
+print("isinstance('str',str) : "+str(isinstance("str",str))+"\n")
+
+#得到类型
+print("type(False) : "+str(type(False)))
