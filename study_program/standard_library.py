62e17804336c85ff51fdf91a40b2e5ce5a7f17c5
code-nick-python <2330458484@qq.com>
2019-05-10 20:20:49 +0800

    add itertools

--- study_program/standard_library.py
+++ study_program/standard_library.py
@@ -20,3 +20,27 @@ print()
 
 print("print : ")
 print(data)
+
+
+
+import itertools
+def run_itertools(func):
+    counter=0
+    for i in func:
+	    print(i)
+	    if counter==9:
+		    break
+	    counter += 1
+print("list(itertools.repeat(10, 3)) : " + str(list(itertools.repeat(10, 3))) + "\n")
+
+print("itertools.cycle('Hello') : ")
+cycle=itertools.cycle("Hello")
+run_itertools(cycle)
+print()
+
+print("itertools.count(10) : ")
+count = itertools.count(10)
+run_itertools(count)
+print()
+
+print("itertools.combinations('1234', 2) : " + str(list(itertools.combinations("1234", 2))))
