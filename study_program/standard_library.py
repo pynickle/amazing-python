aee68ab27ea2cac5ef3acc152616bde17a30b818
code-nick-python <2330458484@qq.com>
2019-05-10 19:41:19 +0800

    add pprint.pprint

--- study_program/standard_library.py
+++ study_program/standard_library.py
@@ -4,3 +4,19 @@ year = int(input("enter the year : "))
 month = int(input("enter the month : "))
 
 print(calendar.month(year,month))
+
+
+
+import pprint
+data = (
+	    "string1",
+	    ("first", "second", "third"),
+		{"hello":"python3", "bye":"python2"},
+	    "string2"
+    ) 
+print("pprint.pprint : ")
+pprint.pprint(data)
+print()
+
+print("print : ")
+print(data)
