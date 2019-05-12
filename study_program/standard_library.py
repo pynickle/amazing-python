b9117249b21b261b3c70b6f86035d12e127f9e85
code-nick-python <2330458484@qq.com>
2019-05-12 15:50:47 +0800

    Add dis module

--- study_program/standard_library.py
+++ study_program/standard_library.py
@@ -88,3 +88,23 @@ pprint.pprint(sys.path)
 print()
 print("getfilesystemencoding : " + sys.getfilesystemencoding() + "\n")
 print("platform : " + sys.platform + "\n")
+
+
+
+import dis
+
+def lcm(num1:int, num2:int)->str:
+	if num1 > num2:
+		counter = num1
+	else:
+		counter = num2
+
+	while True:
+		if counter % num1 == 0 and counter % num2 == 0:
+			lcm = counter
+			break
+		counter += 1
+	return str(lcm)
+
+
+dis.dis("lcm(1,100)")
