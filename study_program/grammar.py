c2ed43fd4ebe5bc005b2e7e5fa2d8b77ed3b9046
code-nick-python <2330458484@qq.com>
2019-05-13 19:18:10 +0800

    Update grammar.py

--- study_program/grammar.py
+++ study_program/grammar.py
@@ -15,3 +15,22 @@ print()
 print("函数注解 : def add(num1:int,num2:int)->int:")
 def add(num1:int,num2:int)->int:
 	return num1+num2
+print()
+
+print("with : ")
+class With():
+	def __enter__(self):
+		print("enter!")
+		return self
+	def operate(self,name,bug):
+		print("operate " + name + "!" + bug)
+	def __exit__(self,exc_type,exc_value,traceback):
+		print("exc_type : " + str(exc_type))
+		print("exc_value : " + str(exc_value))
+		print("traceback : " + str(traceback))
+		print("exit!")
+		return True
+		
+bug=1
+with With() as opr:
+	opr.operate("nick",bug)
