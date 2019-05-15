a25e955d51d3dd4520c22c91e528c139291170e4
code-nick-python <2330458484@qq.com>
2019-05-15 20:27:44 +0800

    Update calculator.py

--- interesting_program/calculator.py
+++ interesting_program/calculator.py
@@ -1,12 +1,35 @@
-symbol=("+","-","*","/","//","**","(",")","1","2","3","4","5","6","7","8","9","0")
+symbol1 = ("+", "-", "*", "/", "//", "**", "(", ")")
+symbol2 = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
 question=input(">>>")
 legal=True
+split_eval=""
+test=""
+point=False
+
 for i in question:
-	if i not in symbol and legal:
-		print("illegal!")
+	if i in symbol1:
+		if not point:
+			split_eval += str(float(int(test)))
+			test=""
+			split_eval += i
+		else:
+			split_eval += test
+			test=""
+			split_eval += i
+	elif i in symbol2:
+		test += i
+	elif i == ".":
+		point = True
+		test += i
+	if i not in symbol1 and i not in symbol2 and legal and i != ".":
+		print(i + " illegal!")
 		legal=False
+
+split_eval += str(float(int(test)))
+		
 if legal:
 	try:
-		print(eval(question))
-	except:
+		print(split_eval + " = " + str(eval(split_eval)))
+	except Exception as e:
+		print(e)
 		print("illegal!")
