a66773c4805eb1941db0362e4bf6b472311b9d5f
code-nick-python <2330458484@qq.com>
2019-05-19 11:02:06 +0800

    Update test.py

--- interesting_program/test.py
+++ interesting_program/test.py
@@ -1,8 +1,36 @@
-def test():
-    import calculator
-    import fibonacci
-    import lcm
-    import mcd
-    import leap_year
-    import password_random
-    import prime_numbers
+def stub_stdin(testcase_inst, inputs):
+    stdin = sys.stdin
+    def cleanup():
+        sys.stdin = stdin
+
+    testcase_inst.addCleanup(cleanup)
+    sys.stdin = io.StringIO(inputs)
+
+def stub_stdout(testcase_inst):
+    stderr = sys.stderr
+    stdout = sys.stdout
+
+    def cleanup():
+        sys.stderr = stderr
+        sys.stdout = stdout
+
+    testcase_inst.addCleanup(cleanup)
+    sys.stderr = io.StringIO()
+    sys.stdout = io.StringIO()
+
+
+class Test(unittest.TestCase):
+	def test(self):
+		stub_stdin(self, '24\n30\n')
+		stub_stdout(self)
+		lcm_test()
+		#print("first:",sys.stdout.getvalue())
+		self.assertEqual(sys.stdout.getvalue(), '120\n')
+		
+		stub_stdin(self, '24\n30\n')
+		stub_stdout(self) # 重置输出
+		mcd_test()
+		#print("second:",sys.stdout.getvalue())
+		self.assertEqual(sys.stdout.getvalue(), '6\n')
+
+unittest.main()
