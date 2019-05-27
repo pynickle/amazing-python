883ab297970ff66ab4b0e7675fffa892f6ed8018
nickle <2330458484@qq.com>
2019-05-27 19:00:45 +0800

    Update prime_numbers.py

--- interesting_program/prime_numbers.py
+++ interesting_program/prime_numbers.py
@@ -1,3 +1,41 @@
+import time
+def time_c(func):
+	def wrapper(*args, **kwargs):
+		#start = time.perf_counter()
+		start = time.time()
+		func(*args, **kwargs)
+		#elapsed = (time.perf_counter() - start)
+		elapsed = (time.time() - start)
+		print("{} : Time used: {}".format(func.__name__, elapsed))
+	return wrapper
+def check_first(number:int):
+	if number < 2:
+		return False
+	elif number == 2:
+		return True
+	return all([number % i for i in range(2, number)])
+
+def check_second(number:int):
+	if number < 2:
+		return False
+	elif number == 2:
+		return True
+	for i in range(2,number//2+1):
+		if number%i==0:
+			return False
+	return True
+num = 7567
+
+@time_c
+def first_test():
+	print(check_first(num))
+	
+@time_c
+def second_test():
+	print(check_second(num))
+first_test()
+second_test()
+
 def prime(lower:int,upper:int)->list:
 	primes=[]
 	for num in range(lower,upper + 1):
@@ -8,8 +46,3 @@ def prime(lower:int,upper:int)->list:
 			else:
 				primes.append(num)
 	return primes
-    
-lower = int(input("enter the lower number : "))
-upper = int(input("enter the upper number : "))
-
-print(prime(lower,upper))
