c1f196a364ec847cedf83680b035b5e91fbcc27b
nickle <2330458484@qq.com>
2019-06-02 09:11:14 +0800

    Update test.py

--- interesting_program/test.py
+++ interesting_program/test.py
@@ -23,23 +23,18 @@ def stub_stdout(testcase_inst):
     sys.stderr = io.StringIO()
     sys.stdout = io.StringIO()
 
+def test_import(input=None, file, result):
+	if input is not None:
+		stub_stdin(self, input)
+	stub_stdout(self)
+	exec("import " + file)
+	self.assertEqual(sys.stdout.getvalue(), result)
 
 class Test(unittest.TestCase):
 	def test(self):
-		stub_stdout(self)
-		import fibonacci
-		self.assertEqual(sys.stdout.getvalue(), '1\n1\n2\n3\n5\n8\n13\n21\n34\n55\n')
+		test_import(file = "fibonacci", result = "1\n1\n2\n3\n5\n8\n13\n21\n34\n55\n")
+		test_import("24\n30\n", "lcm", "120\n")
+		test_import("24\n30\n", "mcd", "6\n")
+		test_import("2000\n", "leap_year", True)
 		
-		stub_stdin(self, '24\n30\n')
-		stub_stdout(self)
-		import lcm
-		#print("first:",sys.stdout.getvalue())
-		self.assertEqual(sys.stdout.getvalue(), '120\n')
-		
-		stub_stdin(self, '24\n30\n')
-		stub_stdout(self) # 重置输出
-		import mcd
-		#print("second:",sys.stdout.getvalue())
-		self.assertEqual(sys.stdout.getvalue(), '6\n')
-
 unittest.main()
