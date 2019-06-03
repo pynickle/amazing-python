a10827b1f787beb17024bdfdc5352d7af408472e
nickle <2330458484@qq.com>
2019-06-03 21:19:41 +0800

    Update test.py

--- interesting_program/test.py
+++ interesting_program/test.py
@@ -24,23 +24,23 @@ def stub_stdout(testcase_inst):
     sys.stderr = io.StringIO()
     sys.stdout = io.StringIO()
 
-def test_import(slf, file, result, input_value = None, second = False):
+def test_import(slf, file, result, input_value = None, original = None):
 	if input is not None:
 		stub_stdin(slf, input_value)
 	stub_stdout(slf)
-	if second:
-		exec("importlib.reload(" + file + "mark)")
+	if original:
+		importlib.reload(original)
 	else:
-		exec("nonlocal " + file + "mark")
 		exec(file + "mark = __import__('" + file + "')")
 	slf.assertEqual(str(sys.stdout.getvalue()), result)
+	exec("return " + file + "mark")
 
 class Test(unittest.TestCase):
 	def test(self):
-		test_import(self, "fibonacci", "1\n1\n2\n3\n5\n8\n13\n21\n34\n55\n")
-		test_import(self, "lcm", "120\n", "24\n30\n")
-		test_import(self, "mcd", "6\n", "24\n30\n")
-		test_import(self, "leap_year", "True\n", "2000\n")
-		test_import(self, "calculator", ">>>3.0*5.0 = 15.0\n", "3*5\n")
-		test_import(self, "calculator", ">>>4.5-3.5*2.0 = -2.5\n", "4.5-3.5*2\n", second = True)
+		fib = test_import(self, "fibonacci", "1\n1\n2\n3\n5\n8\n13\n21\n34\n55\n")
+		lcm = test_import(self, "lcm", "120\n", "24\n30\n")
+		mcd = test_import(self, "mcd", "6\n", "24\n30\n")
+		lea = test_import(self, "leap_year", "True\n", "2000\n")
+		cal = test_import(self, "calculator", ">>>3.0*5.0 = 15.0\n", "3*5\n")
+		test_import(self, "calculator", ">>>4.5-3.5*2.0 = -2.5\n", "4.5-3.5*2\n", original = cal)
 unittest.main()
