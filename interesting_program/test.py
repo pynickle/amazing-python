5b894a9b2d2f525b1c6c2e95ec61951807d5817a
nickle <2330458484@qq.com>
2019-06-03 20:27:50 +0800

    Update test.py

--- interesting_program/test.py
+++ interesting_program/test.py
@@ -1,7 +1,7 @@
 import sys
 import io
 import unittest
-import imp
+import importlib
 
 
 def stub_stdin(testcase_inst, inputs):
@@ -24,12 +24,14 @@ def stub_stdout(testcase_inst):
     sys.stderr = io.StringIO()
     sys.stdout = io.StringIO()
 
-def test_import(slf, file, result, input_value = None):
+def test_import(slf, file, result, input_value = None, second = False):
 	if input is not None:
 		stub_stdin(slf, input_value)
 	stub_stdout(slf)
-	with open(file + ".py", "r") as f:
-		exec(f.read())
+	if second:
+		importlib.reload(file)
+	else:
+		exec("import " + file)
 	slf.assertEqual(str(sys.stdout.getvalue()), result)
 
 class Test(unittest.TestCase):
@@ -39,5 +41,5 @@ class Test(unittest.TestCase):
 		test_import(self, "mcd", "6\n", "24\n30\n")
 		test_import(self, "leap_year", "True\n", "2000\n")
 		test_import(self, "calculator", ">>>3.0*5.0 = 15.0\n", "3*5\n")
-		test_import(self, "calculator", ">>>4.5-3.5*2.0 = -2.5\n", "4.5-3.5*2\n")
+		test_import(self, "calculator", ">>>4.5-3.5*2.0 = -2.5\n", "4.5-3.5*2\n",second = True)
 unittest.main()
