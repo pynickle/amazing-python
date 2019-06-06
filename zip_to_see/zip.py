59a8c95d7d559341c3b9090ec51919ff6ebbae62
nickle <2330458484@qq.com>
2019-06-07 07:51:07 +0800

    Update zip.py

--- zip_to_see/zip.py
+++ zip_to_see/zip.py
@@ -14,10 +14,12 @@ def un_zip(file_name):
                 extracted_path = Path(f.extract(fn))
                 try:
                     extracted_path.rename(fn.encode("cp437").decode("gbk"))
-                except:
+                except Exception as e:
+                    print(e)
                     tkinter.messagebox.showwarning("wrong!", "wrong zip!")
             tkinter.messagebox.showinfo("success!", "finish zip " + str(i) + "!")
-    except:
+    except Exception as e:
+        print(e)
         tkinter.messagebox.showwarning("wrong!", "wrong zip!")
 
 
@@ -27,17 +29,17 @@ def choose_file():
 
 
 def zip_main():
-    top = tkinter.Tk()
     global path, file_choose_1
+    top = tkinter.Tk()
     path = tkinter.StringVar()
+    
     button_begin = tkinter.Button(
         top, text="begin", command=lambda: un_zip(file_name=path)
-    )
-    button_begin.pack()
-    button = tkinter.Button(top, text="choose zip file", command=choose_file)
-    button.pack()
-    file_choose_1 = tkinter.Entry(top, textvariable=path, width=50)
-    file_choose_1.pack()
+    ).pack()
+    
+    button = tkinter.Button(top, text="choose zip file", command=choose_file).pack()
+    
+    file_choose_1 = tkinter.Entry(top, textvariable=path, width=50).pack()
     top.mainloop()
 
 if __name__=="__main__":
