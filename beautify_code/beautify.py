65c33bc90911347885637d3793ad30fd481bb49d
nickle <2330458484@qq.com>
2019-06-07 08:22:12 +0800

    Update beautify.py

--- beautify_code/beautify.py
+++ beautify_code/beautify.py
@@ -9,12 +9,13 @@ def selectPath():
     path_ = askdirectory()
     path.set(path_)
 
-
-def gofmt_file(roots, files, success_files):
+def beautify_file(root, files, file_type, para):
+    para = para.replace("{{root}}", root)
     for file in files:
-        if file.endswith(".go"):
+        if file.endswith(file_type):
             try:
-                call("gofmt -w " + roots + "/" + file)
+                para = para.replace("{{file}}", file)
+                call(para)
                 success_files += 1
                 success.set("success:" + str(success_files))
                 top.update()
@@ -23,75 +24,17 @@ def gofmt_file(roots, files, success_files):
                 continue
     return success_files
 
-
-def gofmt():
-    success_files = 0
-    file_path = path.get()
-    if os.path.exists(file_path):
-        try:
-            for roots, dirs, files in os.walk(file_path):
-                success_files = gofmt_file(roots, files, success_files)
-        except Exception as e:
-            print(e)
-    else:
-        tkinter.messagebox.showerror("wrong!", "path wrong!")
-
-
-def autopep8_file(root, files, success_files):
-    for file in files:
-        if file.endswith(".py"):
-            try:
-                call("autopep8 --in-place " + root + "/" + file, shell=True)
-                success_files += 1
-                success.set("success:" + str(success_files))
-                top.update()
-            except Exception as e:
-                print(e)
-                continue
-    return success_files
-
-
-def autopep8():
+def beautify(file_type, para):
     success_files = 0
     file_path = path.get()
     if os.path.exists(file_path):
         try:
             for root, dirs, files in os.walk(file_path):
-                success_files = autopep8_file(root, files, success_files)
+                success_files = beautify_file(root, files, file_type, para)
         except Exception as e:
             print(e)
-            tkinter.messagebox.showerror("wrong!", "path wrong!")
     else:
         tkinter.messagebox.showerror("wrong!", "path wrong!")
-
-
-def black_file(root, files, success_files):
-    for file in files:
-        if file.endswith(".py"):
-            try:
-                call("black " + root + "/" + file, shell=True)
-                success_files += 1
-                success.set("success:" + str(success_files))
-                top.update()
-            except Exception as e:
-                print(e)
-                continue
-    return success_files
-
-
-def black():
-    success_files = 0
-    file_path = path.get()
-    if os.path.exists(file_path):
-        try:
-            for root, dirs, files in os.walk(file_path):
-                success_files = black_file(root, files, success_files)
-        except Exception as e:
-            print(e)
-            tkinter.messagebox.showerror("wrong!", "black wrong!")
-    else:
-        tkinter.messagebox.showerror("wrong!", "path wrong!")
-
 
 def main():
     global path, success, top
@@ -104,9 +47,12 @@ def main():
     success = tkinter.StringVar()
     success.set("success:")
 
-    button1 = tkinter.Button(top, text="begin autopep8", command=autopep8).pack()
-    button2 = tkinter.Button(top, text="begin black", command=black).pack()
-    button3 = tkinter.Button(top, text="begin gofmt", command=gofmt).pack()
+    button1 = tkinter.Button(top, text="begin autopep8", 
+                             command=lambda:beautify(".py", "autopep8 --in-place {{root}}/{{file}}").pack()
+    button2 = tkinter.Button(top, text="begin black",
+                             command=lambda:beautify(".py", "black {{root}}/{{file}}").pack()
+    button3 = tkinter.Button(top, text="begin gofmt", 
+                             command=lambda:beautify(".go", "gofmt -w {{root}}/{{file}}").pack()
 
     choose_path = tkinter.Button(top, text="choose path", command=selectPath).pack()
     path_show = tkinter.Entry(top, textvariable=path, width=150).pack()
