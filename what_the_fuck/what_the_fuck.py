25ed54248ec1b3fa27a34b06d0119ebf7d2f7bd5
nickle <2330458484@qq.com>
2019-06-16 02:16:38 +0800

    Update what_the_fuck.py

--- what_the_fuck/what_the_fuck.py
+++ what_the_fuck/what_the_fuck.py
@@ -126,3 +126,33 @@ except Exception as e:
         del e
 That's terrible!Give back my e!
 """
+
+
+
+"""
+Let me look at your trick!
+Let me bet, except list_2, others is all empty!
+"""
+list_1 = [1, 2, 3, 4]
+list_2 = [1, 2, 3, 4]
+list_3 = [1, 2, 3, 4]
+list_4 = [1, 2, 3, 4]
+
+for idx, item in enumerate(list_1):
+    del item
+
+for idx, item in enumerate(list_2):
+    list_2.remove(item)
+
+for idx, item in enumerate(list_3[:]):
+    list_3.remove(item)
+
+for idx, item in enumerate(list_4):
+    list_4.pop(idx)
+print(list_1, list_2, list_3, list_4)
+"""
+You are wrong again...
+actually list_2 and list_4 is [2,4]...
+When you remove an item from a list,the length is changed,but the index is not changed.
+That's why 1 and 3 is removed, but 2 and 4 survived...
+"""
