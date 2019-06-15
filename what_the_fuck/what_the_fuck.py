6ee402e5facc157833687980b096f8bc4344c9ac
nickle <2330458484@qq.com>
2019-06-16 00:36:58 +0800

    add constant folding

--- what_the_fuck/what_the_fuck.py
+++ what_the_fuck/what_the_fuck.py
@@ -15,3 +15,21 @@ How can that happen!
 Actually,when break, continue or return in try sentence, and exactly there is a finally sentence...
 It will run finally first!
 """
+
+
+
+"""
+l thought l would see two True on the screen...
+"""
+print('what' * 5 is "whatwhatwhatwhatwhat")
+print('what' * 6 is "whatwhatwhatwhatwhatwhat")
+"""
+But actually...
+One True and One False
+How can that happan!
+eee...Because of the existance of constant folding...
+a powerful technology...
+constant folding in python will convert a string with less than twenty characters.
+But if the string has more than two characters, python will not convert it.
+Think about pyc's feeling!
+"""
