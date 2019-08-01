f24710d4959b89fb8dea96c08a49fe5bd4598bba
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-08-01 09:37:00 +0800

    add test

---
+++ Tools/english_word/test/test_flask.py
@@ -0,0 +1,42 @@
+import os
+import tempfile
+import sys
+
+path = "\\".join(__file__.split("\\")[:-1]) + "/../"
+sys.path.append(path)
+
+import pytest
+
+import app
+
+@pytest.fixture
+def client():
+    db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+    app.app.config['TESTING'] = True
+    client = app.app.test_client()
+
+    with app.app.app_context():
+        app.db.create_all()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(app.app.config['DATABASE'])
+
+def test_empty_db(client):
+    rv = client.get('/')
+    assert '词神' in rv.data.decode(encoding="utf-8")
+
+def test_add_new_word(client):
+    rv = client.post("/add-new-word", data = dict(
+        data = "book n. 书"
+    ), follow_redirects = True)
+    assert 'book' in rv.data.decode(encoding="utf-8")
+    assert 'n.' in rv.data.decode(encoding="utf-8")
+    assert '书' in rv.data.decode(encoding="utf-8")
+
+def test_recite_word(client):
+    rv = client.post("/recite-words", data = dict(
+        data = "book"
+    ), follow_redirects = True)
+    assert '答对咯！' in rv.data.decode(encoding="utf-8")
